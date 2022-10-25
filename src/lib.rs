f3738fcecd4a639c7237f37ce99cf90cfcddf882
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2022-10-24 23:40:53 -0300

    feat: tree adds nodes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct TreeNode<T> {
+    pub value: T,
+    nodes: Vec<TreeNode<T>>,
+}
+
+impl<T> TreeNode<T> {
+    pub fn new(value: T) -> Self {
+        TreeNode { value, nodes: Vec::new() }
+    }
+
+    pub fn add_node(&mut self, value: T) {
+        self.nodes.push(TreeNode::new(value));
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +18,19 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn creates_tree() {
+        let tree = TreeNode::new(10);
+
+        assert_eq!(tree.value, 10);
+    }
+
+    #[test]
+    fn tree_add_nodes() {
+        let mut tree = TreeNode::new(2);
+
+        tree.add_node(5);
+        tree.add_node(4);
+
+        assert_eq!(tree.nodes.len(), 2)
     }
 }
